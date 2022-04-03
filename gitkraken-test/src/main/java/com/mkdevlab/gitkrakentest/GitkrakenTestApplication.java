869dd51de3b782dfd8e7b1c21a128ea003d667a0
myung kyu chung <wjdaudrb7@naver.com>
2022-04-04 00:04:50 +0900

    gitkraken test

---
+++ gitkraken-test/src/main/java/com/mkdevlab/gitkrakentest/GitkrakenTestApplication.java
@@ -0,0 +1,13 @@
+package com.mkdevlab.gitkrakentest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitkrakenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitkrakenTestApplication.class, args);
+	}
+
+}
---
+++ gitkraken-test/src/test/java/com/mkdevlab/gitkrakentest/GitkrakenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkdevlab.gitkrakentest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitkrakenTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
